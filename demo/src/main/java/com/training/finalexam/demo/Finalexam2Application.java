de71b3154b6d43cdeb6a73829ac3d3f4f36da749
Itzel Ramirez <itzel.ramirez@Seguritech.local>
2019-05-31 16:07:21 -0500

    primersb

---
+++ demo/src/main/java/com/training/finalexam/demo/Finalexam2Application.java
@@ -0,0 +1,13 @@
+package com.training.finalexam.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Finalexam2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Finalexam2Application.class, args);
+	}
+
+}
